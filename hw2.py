4ee5f58efea9aea3d172a487a4259cf97d9bec23
Ali Bhujwala <ali.bhujwala@capitalone.com>
2017-03-31 18:18:25 -0500

    adding in, innerjoin, antijoin, outerjoin

--- hw2.py
+++ hw2.py
@@ -24,11 +24,12 @@ for i,row in enumerate(csv.reader(open(manifest_fpath,'r'))):
         manifest_schema=row
     else:
         manifest_table.append(row)
-print "STARTING TABLE:"
+print "STUDENTS TABLE:"
 print students_schema
 print students_table
-# print manifest_schema
-# print manifest_table
+print "MANIFEST TABLE:"
+print manifest_schema
+print manifest_table
 def groupBy(schema,table,groupByfield):
     if groupByfield not in schema:
         raise ValueError("GroupBy field not found in schema")
@@ -43,10 +44,29 @@ def groupBy(schema,table,groupByfield):
     for v,r in sorted(temp_row):
         results.append(r)
     return results
-# print "GROUP BY QUERY RESULT:"
-# print groupBy(students_schema,students_table,'GPA')
+print
+print "QUERY: SELECT * FROM students GROUP BY 'GPA'"
+print "FUNCTION: groupBy(students_schema,students_table,'GPA')"
+print "OUTPUT:"
+print groupBy(students_schema,students_table,'GPA')
 
-def leftJoin(table1,table1_schema,table2,table2_schema,joinOnField):
+def inn(schema,table,field,accepted_values):
+    if field not in schema:
+        raise ValueError("field not found in schema")
+    else:
+        index = schema.index(field)
+    ret = []
+    for l in table:
+        if l[index] in accepted_values:
+            ret.append(l)
+    return ret
+print
+print "QUERY: SELECT * FROM students where lastName IN ('Bhujwala','Downey')"
+print "FUNCTION: inn(students_schema,students_table,'lastName',['Bhujwala','Downey'])"
+print "OUTPUT:"
+print inn(students_schema,students_table,'lastName',['Bhujwala','Downey'])
+
+def innerJoin(table1,table1_schema,table2,table2_schema,joinOnField):
     if joinOnField not in table1_schema or joinOnField not in table2_schema:
         raise ValueError("joinOn field not found in schema")
     table1_join_index = table1_schema.index(joinOnField)
@@ -59,15 +79,16 @@ def leftJoin(table1,table1_schema,table2,table2_schema,joinOnField):
             if l[table1_join_index] == l2[table2_join_index]:
                 #hardcode for query, grab second element aka email.
                 r = l2[1]
-        ret.append(l+[r])
+        if r != '':
+            ret.append(l+[r])
     return ret
-print """
-QUERY: SELECT students.firstName, students.lastName, students.GPA, manifest.email
+print
+print """QUERY: SELECT students.firstName, students.lastName, students.GPA, manifest.email
 FROM students
-left join manifest on students.lastName = manifest.lastName;
-"""
-
-print leftJoin(students_table,students_schema,manifest_table,manifest_schema,"lastName")
+inner join manifest on students.lastName = manifest.lastName;"""
+print "FUNCTION: innerJoin(students_table,students_schema,manifest_table,manifest_schema,'lastName')"
+print "OUTPUT:"
+print innerJoin(students_table,students_schema,manifest_table,manifest_schema,'lastName')
 
 def antiJoin(table1,table1_schema,table2,table2_schema,joinOnField):
     table1_join_index = table1_schema.index(joinOnField)
@@ -83,9 +104,41 @@ def antiJoin(table1,table1_schema,table2,table2_schema,joinOnField):
         if r is '':
             ret.append(l+[r])
     return ret
+
 print """
 QUERY: SELECT students.firstName, students.lastName, students.GPA
 FROM students
-WHERE NOT EXISTS (SELECT * FROM manifest WHERE manfiest.lastName = students.lastName)
-"""
-print antiJoin(students_table,students_schema,manifest_table,manifest_schema,"lastName")
+WHERE NOT EXISTS (SELECT * FROM manifest WHERE manfiest.lastName = students.lastName)"""
+print "FUNCTION: antiJoin(students_table,students_schema,manifest_table,manifest_schema,'lastName')"
+print "OUTPUT:"
+print antiJoin(students_table,students_schema,manifest_table,manifest_schema,'lastName')
+
+def outerJoin(table1,table1_schema,table2,table2_schema,joinOnField):
+    table1_join_index = table1_schema.index(joinOnField)
+    table2_join_index = table2_schema.index(joinOnField)
+    ret = []
+    d = {}
+    for l in table1:
+        #find table2 record
+        r = ''
+        for l2 in table2:
+            if l[table1_join_index] == l2[table2_join_index]:
+                #hardcode for query, grab second element aka email.
+                d[l[table1_join_index]] = True
+                r = l2[1]
+        if r is not '':
+            ret.append(l+[r])
+        else:
+            ret.append(l+[''])
+    for l in table2:
+        if l[0] not in d:
+            ret.append(['']+[l[0]]+['']+[l[1]])
+    return ret
+
+print """
+QUERY: SELECT *
+FROM students
+FULL OUTER JOIN manifest on students.lastName = manifest.lastName;"""
+print "FUNCTION: outerJoin(students_table,students_schema,manifest_table,manifest_schema,'lastName')"
+print "OUTPUT:"
+print outerJoin(students_table,students_schema,manifest_table,manifest_schema,'lastName')
